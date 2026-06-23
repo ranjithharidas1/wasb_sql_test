09547222c6a7363442db7e3732775807812fcf68
ranjithharidas1 <46494513+ranjithharidas1@users.noreply.github.com>
2026-06-23 22:17:34 +0200

    Update create_expenses.sql

--- create_expenses.sql
+++ create_expenses.sql
@@ -16,25 +16,26 @@
 
 USE memory.default;
 
-CREATE TABLE EXPENSE AS
-SELECT
-    employee_id,
-    unit_price,
-    quantity
-FROM (
-    VALUES
-        -- drinkies.txt: Alex Jacobson - "Drinks, lots of drinks"
-        (CAST(3 AS TINYINT), CAST(6.50  AS DECIMAL(8,2)), CAST(14 AS TINYINT)),
-        -- drinks.txt: Alex Jacobson - "More Drinks"
-        (CAST(3 AS TINYINT), CAST(11.00 AS DECIMAL(8,2)), CAST(20 AS TINYINT)),
-        -- drinkss.txt: Alex Jacobson - "So Many Drinks!"
-        (CAST(3 AS TINYINT), CAST(22.00 AS DECIMAL(8,2)), CAST(18 AS TINYINT)),
-        -- duh_i_think_i_got_too_many.txt: Alex Jacobson - "I bought everyone in the bar a drink!"
-        (CAST(3 AS TINYINT), CAST(13.00 AS DECIMAL(8,2)), CAST(75 AS TINYINT)),
-        -- i_got_lost_on_the_way_home_and_now_im_in_mexico.txt: Andrea Ghibaudi - "Flights from Mexico back to New York"
-        (CAST(9 AS TINYINT), CAST(300.00 AS DECIMAL(8,2)), CAST(1  AS TINYINT)),
-        -- ubers.txt: Darren Poynton - "Ubers to get us all home"
-        (CAST(4 AS TINYINT), CAST(40.00 AS DECIMAL(8,2)), CAST(9  AS TINYINT)),
-        -- we_stopped_for_a_kebabs.txt: Umberto Torrielli - "I had too much fun and needed something to eat"
-        (CAST(2 AS TINYINT), CAST(17.50 AS DECIMAL(8,2)), CAST(4  AS TINYINT))
-) AS t(employee_id, unit_price, quantity);
+DROP TABLE IF EXISTS EXPENSE;
+
+CREATE TABLE EXPENSE (
+    employee_id TINYINT,
+    unit_price  DECIMAL(8, 2),
+    quantity    TINYINT
+);
+
+INSERT INTO EXPENSE VALUES
+    -- drinkies.txt: Alex Jacobson - "Drinks, lots of drinks"
+    (CAST(3 AS TINYINT), CAST(6.50  AS DECIMAL(8,2)), CAST(14 AS TINYINT)),
+    -- drinks.txt: Alex Jacobson - "More Drinks"
+    (CAST(3 AS TINYINT), CAST(11.00 AS DECIMAL(8,2)), CAST(20 AS TINYINT)),
+    -- drinkss.txt: Alex Jacobson - "So Many Drinks!"
+    (CAST(3 AS TINYINT), CAST(22.00 AS DECIMAL(8,2)), CAST(18 AS TINYINT)),
+    -- duh_i_think_i_got_too_many.txt: Alex Jacobson - "I bought everyone in the bar a drink!"
+    (CAST(3 AS TINYINT), CAST(13.00 AS DECIMAL(8,2)), CAST(75 AS TINYINT)),
+    -- i_got_lost_on_the_way_home_and_now_im_in_mexico.txt: Andrea Ghibaudi
+    (CAST(9 AS TINYINT), CAST(300.00 AS DECIMAL(8,2)), CAST(1  AS TINYINT)),
+    -- ubers.txt: Darren Poynton - "Ubers to get us all home"
+    (CAST(4 AS TINYINT), CAST(40.00 AS DECIMAL(8,2)), CAST(9  AS TINYINT)),
+    -- we_stopped_for_a_kebabs.txt: Umberto Torrielli
+    (CAST(2 AS TINYINT), CAST(17.50 AS DECIMAL(8,2)), CAST(4  AS TINYINT));
